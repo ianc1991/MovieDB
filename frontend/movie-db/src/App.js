782d5d2b3f5cf3a0412a481f03f99fb0960aa404
Ian <ianmchristian@gmail.com>
2021-12-21 17:11:30 -0500

    installed bootstrap. added navbar.

---
+++ frontend/movie-db/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// Components
+import Navbar from './components/Navbar';
+// Assets
+import background from './assets/background.jpg';
+
+function App() {
+  return (
+    <div className="appC">
+      <header className="headerC">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <Navbar />
+      </header>
+      <body>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
