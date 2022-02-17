fd6706316ec87df99a53cee756f17249ecb4dc88
Ian <ianmchristian@gmail.com>
2022-02-17 12:50:22 -0500

    fixed logo navigate

--- frontend/movie-db/src/components/Navbar/Navbar.js
+++ frontend/movie-db/src/components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     return (
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href='/'>MovieDB</a>
+                    <a className="navbar-brand" onClick={() => navigate('/')}>MovieDB</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
